68dceea4e4423d2d11495998382fe22a549fbec3
rootuserrafik <contactwithrafik@gmail.com>
2022-02-24 22:28:43 +0600

    added a listen port for run server address

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const webServer = http.createServer(app);
+
+
+
+
+
+
+
+// Add a 
+const port = 8080;
+webServer.listen(port, function(){
+    console.log('Server port is: '+port);
+})
